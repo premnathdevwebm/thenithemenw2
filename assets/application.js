6b4dbeb0b31ad4eec7065f23b43ad12aa4a6ba81
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-09-12 01:48:17 +0530

    thenitheme

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,29 @@
 // Put your application javascript here
+document.addEventListener("DOMContentLoaded", () => {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".carousel-slides img");
+  if (slides.length) {
+    const totalSlides = slides.length;
+    const slideWidth = slides[0].clientWidth;
+
+    setInterval(() => {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      moveCarouselTo(currentSlide);
+    }, 3000);
+
+    function moveCarouselTo(slide) {
+      const slideTrack = document.querySelector(".carousel-slides");
+      slideTrack.style.transform = "translateX(-" + slide * slideWidth + "px)";
+    }
+  }
+
+  const cartButton = document.getElementById("cartclick");
+  cartButton.addEventListener("click", function () {
+    window.location.href = "/cart";
+  });
+
+  const loginButton = document.getElementById("loginclick");
+  loginButton.addEventListener("click", function () {
+    window.location.href = "/account/login";
+  });
+});
